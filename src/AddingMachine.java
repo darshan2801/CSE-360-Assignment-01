6a01ff1fc6a65bb83c1df11f8bfd63a82cb591fa
darsh <darshan2801@gmail.com>
2020-10-03 13:16:43 -0700

    inital commit

---
+++ src/AddingMachine.java
@@ -0,0 +1,25 @@
+
+public class AddingMachine {
+    private int total;
+
+    public AddingMachine() {
+        total = 0;  // not needed - included for clarity
+    }
+
+    public int getTotal() {
+        return 0; // method returns the total
+    }
+
+    public void add(int value) {
+    } // method adds desired value to total variable
+
+    public void subtract(int value) {
+    } // method subtracts desired value to total variable
+
+    public String toString() {
+        return "";
+    } // method keeps a history of all actions done
+
+    public void clear() {
+    }
+} // clears values
